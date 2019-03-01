2afbd7af038620e5c74a2404cbd854f318807288
wjchulme <25637345+wjchulme@users.noreply.github.com>
2019-03-01 14:10:03 +0000

    Create install.R

---
+++ install.R
@@ -0,0 +1,13 @@
+install.packages("devtools")
+install.packages("tidyverse")
+install.packages("httr")
+install.packages("readxl")
+install.packages("broom")
+
+install.packages("survival")
+install.packages("relsurv")
+
+install.packages("scales")
+install.packages('ggmosaic')
+
+install_github("wjchulme/willsutils")
